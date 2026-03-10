de5b6e1ef174858582fe8e16fa13fabca1b2c66b
Caleb Reeves <calebreeves@fortune-cookie.lcl>
2026-03-10 09:00:01 -0400

    Cronjob'd commit in LocalDrive

---
+++ db/linked-list.h
@@ -0,0 +1,14 @@
+#ifndef LINKED_LIST_H_
+#define LINKED_LIST_H_
+
+#define true 1
+#define false 0
+
+#define LL_FILENAME_TOO_LONG_ERROR 1
+#define LL_NOT_HEAD_NODE 2
+#define LL_bAD_INDEX 3
+#define LL_WRONG_NODE 4
+
+struct llNode;
+
+#endif
