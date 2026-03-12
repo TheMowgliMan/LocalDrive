a051b8ac9fc0c26278ae03a8a2136ca9570b4d6d
Caleb Reeves <calebreeves@fortune-cookie.lcl>
2026-03-12 16:40:01 -0400

    Cronjob'd commit in LocalDrive

--- db/fileio.c
+++ db/fileio.c
@@ -1,4 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#include "util.h"
-#include
+#include "../util.h"
+#include "linked-list.h"
+
+struct userWrapper;
+
+struct userWrapper {
+  struct llNode* user_meta;
+  struct llNode* user_files;
+};
+
+// Used to link users together
+struct llNode* users_meta_head = NULL;
+
+struct userWrapper* generate_user(char* name) {
+  struct userWrapper* u = (struct userWrapper*)xmalloc(sizeof(struct userWrapper),
+													   "struct userWrapper* generate_user() @ fileio.c");
+
+  append(users_meta_head, name, 0, 0);
+  u->user_meta = prev_node(users_meta_head);
+  u->user_files = new_ll();
+
+  return u;
+}
+
+int initialize() {
+  users_meta_head = new_ll();
+  
+  // We use an array of users because it doesn't change often
+  // ...maybe make it a linked list?
+  struct userWrapper* users[1];
+  users[1] = generate_user("root");
+}
+
+// TODO: generate a way to keep track of the user folder, hash the users name as a folder name?
+// Or just use a counter?
